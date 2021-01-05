3d121e9d8cb2e42c778df4d1a607824b4c69e19b
Frederik Bolding <frederik.bolding@gmail.com>
2021-01-05 13:21:15 +0100

    Fix a bunch of stories

--- src/components/NetworkNodeDropdown.stories.tsx
+++ src/components/NetworkNodeDropdown.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { ProvidersWrapper } from 'test-utils';
 
 import { DEFAULT_NETWORK } from '@config';
 
@@ -22,7 +23,11 @@ const withAdd = () => (
 );
 
 storiesOf('NetworkNodesDropdown', module)
-  .addDecorator((story) => <AppProviders>{story()}</AppProviders>)
+  .addDecorator((story) => (
+    <ProvidersWrapper>
+      <AppProviders>{story()}</AppProviders>
+    </ProvidersWrapper>
+  ))
   .add('Select', () => selectOnly(), {
     design: {
       type: 'figma',
